08a25974f7510cf72625d1cf384fefbecf7b7811
mohamed magdy saber <momagdys194@gmail.com>
2024-02-08 05:12:03 +0200

    finesh jop fair

--- lib/core/utils/app_images/app_images.dart
+++ lib/core/utils/app_images/app_images.dart
@@ -12,8 +12,10 @@ class AppImages
   static const String gift = "assets/images/svgs/gift.svg";
   static const String gift2 = "assets/images/svgs/Component 4.svg";
   static const String profile = "assets/images/svgs/profile.svg";
-
-
+  static const String upload = "assets/images/svgs/upload.svg";
+  static const String pdfIcon = "assets/images/svgs/pdf_icon.svg";
+  static const String userProfileImage = "assets/images/svgs/user_profile_image.png";
+  static const String qrIcon = "assets/images/svgs/qr_icon.svg";
 
   /// pngs
   static const String cutLogo = "assets/images/pngs/اليوم_الهندسي__1___1__page-0002-removebg-preview 6.png";
